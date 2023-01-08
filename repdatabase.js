aa8feb81fb5de1287e59322b8b24a9d699e76bea
btxuyenhcmus <btxuyenhcmus@gmail.com>
2023-01-08 22:43:59 +0700

    [INIT] init project

--- repdatabase.js
+++ repdatabase.js
@@ -4,7 +4,7 @@ require('dotenv').config({ path: path.join(__dirname, '.env') });
 
 // enviroment variable is set in User Data when lauching EC2 instance
 const connection = mysql.createPool({
-    host: "read.replica.local",
+    host: "database-2.crsi7sigzyqk.ap-southeast-1.rds.amazonaws.com",
     port: process.env.PORT_DATABASE,
     database: process.env.DATABASE_BASE,
     user : process.env.USER_DATABASE,
